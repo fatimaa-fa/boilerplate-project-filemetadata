1e18c4771ecdf2e063b2709b8d0b17daf68aadb4
fatima aa <alashkaffatima@gmail.com>
2025-05-04 16:34:49 +0300

    commit 1

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+//added code 
+var multer = require('multer');
+var upload = multer();
+
 var app = express();
 
 app.use(cors());
@@ -11,8 +15,18 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+//added code
+// POST /api/fileanalyse
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const file = req.file;
+  if (!file) return res.status(400).send('No file uploaded');
+  
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
